06a208c00a201fd59ab3e2c6a0e71a5d6aa70162
Zawarkich <santotskyilya@gmail.com>
2025-10-02 13:18:39 +0300

    Return HTML group list at root

--- src/main/java/gde/gde_search/controller/GdeController.java
+++ src/main/java/gde/gde_search/controller/GdeController.java
@@ -8,7 +8,17 @@ public class GdeController {
 
     @GetMapping("/")
     public String root() {
-        return "hello world";
+        return "<html><head><meta charset=\"utf-8\"></head><body>"
+                + "<h1>список группы</h1>"
+                + "<table border=\"1\" cellpadding=\"6\">"
+                + "<thead><tr><th>ID</th><th>ФИО</th><th>Взвод</th><th>группа</th><th>местонахождение</th></tr></thead>"
+                + "<tbody>"
+                + "<tr><td>1</td><td>Головатенко</td><td>4</td><td>334701</td><td>-</td></tr>"
+                + "<tr><td>2</td><td>Козлов</td><td>4</td><td>334701</td><td>-</td></tr>"
+                + "<tr><td>3</td><td>Королёв</td><td>4</td><td>334701</td><td>-</td></tr>"
+                + "<tr><td>4</td><td>Ленько</td><td>4</td><td>334701</td><td>-</td></tr>"
+                + "<tr><td>5</td><td>Сантоцкий</td><td>4</td><td>334701</td><td>-</td></tr>"
+                + "</tbody></table></body></html>";
     }
 
 }
